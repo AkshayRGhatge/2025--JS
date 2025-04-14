424c150252de92f06f58b90560c3ee3c63bf76bd
Akshay <akshayghatge6011@gmail.com>
2025-04-14 07:41:53 -0400

    Added moneyTest.js using jasmine and removed the unnecessary default code from jasmine

---
+++ javascript-amazon-project-main/tests-jasmine/moneyTest.js
@@ -0,0 +1,20 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+//Describe is the test suite name 
+describe('Test Suite: Format Currency',()=>{
+
+    //it is the test 
+    it('convert cents into dollars',()=>{
+
+        //Expect: Calling the actual method and compare the value
+        //Expect give us an object
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+
+    it('works with 0',()=>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+
+    it('round up to nearest cemt',()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+})
